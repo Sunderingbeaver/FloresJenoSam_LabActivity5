05566d68e34d60d92f79b694a19ad4ccc50a1ce5
Jeño <jenosam17@gmail.com>
2022-12-16 04:01:52 +0800

    added exception where it asks for a year if the input is not a number
    changed values for all elements
    
    Extends Jframe added
    
    Constructor set

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -1,6 +1,10 @@
 package SimpleCalc;
 
+import LeapYear.LeapYearGUI;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SimpleCalcGUI extends JFrame{
     private JPanel panel1;
@@ -9,7 +13,15 @@ public class SimpleCalcGUI extends JFrame{
     private JButton btnCompute;
     private JTextField tfNumber2;
     private JTextField lblResult;
+    public SimpleCalcGUI(String title){
+        super(title);
 
-
-
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(panel1);
+        this.pack();
+    }
+    public static void main(String[] args){
+        JFrame a = new SimpleCalcGUI("Simple Calculator");
+        a.setVisible(true);
+    }
 }
