216c9cabd91d2e7444b6d55d87d4b67cae356533
Jeño <jenosam17@gmail.com>
2022-12-16 04:56:17 +0800

    changed name for checkbox and radio button

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -1,10 +1,36 @@
 package FoodOrdering;
 
+import SimpleCalc.SimpleCalcGUI;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class FoodOrderGUI {
     private JPanel panel1;
-    private JCheckBox pizzaCheckBox;
-    private JRadioButton noneRadioButton;
+    private JCheckBox cPizza;
+    private JRadioButton rnNone;
     private JButton orderButton;
+    private JCheckBox cBurder;
+    private JCheckBox cFries;
+    private JCheckBox cSoftDrinks;
+    private JPanel cTea;
+    private JCheckBox cSundae;
+    private JRadioButton rb5;
+    private JRadioButton rb10;
+    private JRadioButton rb15;
+
+    public FoodOrderGUI() {
+        orderButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+    public static void main(String[] args){
+        JFrame a = new SimpleCalcGUI("Food Ordering System");
+        a.setVisible(true);
+    }
 }
