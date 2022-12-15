f27808faee87243eedb0b527ec68357f23de636d
Jeño <jenosam17@gmail.com>
2022-12-16 03:38:43 +0800

    added exception where it asks for a year if the input is not a number

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -1,9 +1,60 @@
 package LeapYear;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class LeapYearGUI {
+public class LeapYearGUI extends JFrame{
     private JPanel panel1;
-    private JTextField textField1;
-    private JButton checkButton;
+    private JTextField tfYear;
+    private JButton btnCheckYear;
+
+    public LeapYearGUI(String title){
+        super(title);
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(panel1);
+        this.pack();
+        btnCheckYear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //JField text to double
+                int tempyear = 0;
+                boolean bool = true;
+                do {
+                    try {
+                        tempyear = (int) (Double.parseDouble(tfYear.getText()));
+                    } catch (Exception ex) {
+                        JOptionPane.showMessageDialog(null, "Please enter a number!");
+                        bool = false;
+                    }
+                    if(bool == false){
+                        break;
+                    }
+                    } while(bool == false);
+                    if(tempyear%100 != 0 && bool != false) {
+                        int year = tempyear % 4;
+                        if (year == 0) {
+                            JOptionPane.showMessageDialog(null, "Leap Year");
+                        } else {
+                            JOptionPane.showMessageDialog(null, "Not a leap Year");
+                        }
+                    } else if (tempyear%100==0 && bool != false){
+                        int year = tempyear % 400;
+                        if (year == 0) {
+                            JOptionPane.showMessageDialog(null, "Leap Year");
+                        } else {
+                            JOptionPane.showMessageDialog(null, "Not a leap Year");
+                        }
+                    }
+            }
+        });
+    }
+    public static void main(String[] args){
+        JFrame a = new LeapYearGUI("Leap Year Checker");
+        a.setVisible(true);
+    }
 }
+
+
