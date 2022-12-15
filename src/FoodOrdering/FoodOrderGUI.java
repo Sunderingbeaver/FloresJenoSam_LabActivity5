82b2b2fd5e16363d481b1dfd801765ec5a3c0edb
Jeño <jenosam17@gmail.com>
2022-12-16 05:22:36 +0800

    changed name for checkbox and radio button
    discount radio button added into a button group
    
    If statement for all checkbox and for the options in the radio button group

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -1,18 +1,18 @@
 package FoodOrdering;
 
-import SimpleCalc.SimpleCalcGUI;
-
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class FoodOrderGUI extends JFrame{
     private JPanel panel1;
     private JCheckBox cPizza;
     private JRadioButton rbNone;
-    private JButton orderButton;
+    private JButton btnOrder;
     private JCheckBox cBurger;
     private JCheckBox cFries;
     private JCheckBox cSoftDrinks;
-    private JPanel cTea;
+    private JCheckBox cTea;
     private JCheckBox cSundae;
     private JRadioButton rb5;
     private JRadioButton rb10;
@@ -24,6 +24,37 @@ public class FoodOrderGUI extends JFrame{
         this.setContentPane(panel1);
         this.pack();
 
+        btnOrder.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            double sum = 0;
+            boolean flag = true;
+                if(cPizza.isSelected()){
+                    sum+=100;
+                }if(cBurger.isSelected()){
+                    sum+=80;
+                }if(cFries.isSelected()){
+                    sum+=65;
+                }if(cSoftDrinks.isSelected()){
+                    sum+=55;
+                }if(cTea.isSelected()){
+                    sum+=50;
+                }if(cSundae.isSelected()) {
+                    sum += 40;
+                }if(rbNone.isSelected()){
+                    sum = sum;
+                }if(rb5.isSelected()){
+                    double minus = sum * .05;
+                    sum=sum-minus;
+                }if(rb10.isSelected()){
+                    double minus = sum * .1;
+                    sum=sum-minus;
+                }if(rb15.isSelected()){
+                    double minus = sum * .15;
+                    sum = sum-minus;
+                }
+            }
+        });
     }
 
     public static void main(String[] args){
