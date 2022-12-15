a7a6d07107f9c56198b56de1f0c98771c9206df2
Jeño <jenosam17@gmail.com>
2022-12-16 05:41:57 +0800

    changed name for checkbox and radio button
    discount radio button added into a button group
    
    If statement for all checkbox and for the options in the radio button group
    
    Added Option Pane for the result
    
    Exception handling in the case where no order was selected

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -3,6 +3,7 @@ package FoodOrdering;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.nio.channels.IllegalSelectorException;
 
 public class FoodOrderGUI extends JFrame{
     private JPanel panel1;
@@ -27,33 +28,52 @@ public class FoodOrderGUI extends JFrame{
         btnOrder.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-            double sum = 0;
-            boolean flag = true;
-                if(cPizza.isSelected()){
-                    sum+=100;
-                }if(cBurger.isSelected()){
-                    sum+=80;
-                }if(cFries.isSelected()){
-                    sum+=65;
-                }if(cSoftDrinks.isSelected()){
-                    sum+=55;
-                }if(cTea.isSelected()){
-                    sum+=50;
-                }if(cSundae.isSelected()) {
-                    sum += 40;
-                }if(rbNone.isSelected()){
-                    sum = sum;
-                }if(rb5.isSelected()){
-                    double minus = sum * .05;
-                    sum=sum-minus;
-                }if(rb10.isSelected()){
-                    double minus = sum * .1;
-                    sum=sum-minus;
-                }if(rb15.isSelected()){
-                    double minus = sum * .15;
-                    sum = sum-minus;
-                }
-                JOptionPane.showMessageDialog(null, String.format("%.2f", sum));
+                double sum = 0;
+                boolean bool = true;
+                do {
+                    try {
+                        if (cPizza.isSelected()) {
+                            sum += 100;
+                        }
+                        if (cBurger.isSelected()) {
+                            sum += 80;
+                        }
+                        if (cFries.isSelected()) {
+                            sum += 65;
+                        }
+                        if (cSoftDrinks.isSelected()) {
+                            sum += 55;
+                        }
+                        if (cTea.isSelected()) {
+                            sum += 50;
+                        }
+                        if (cSundae.isSelected()) {
+                            sum += 40;
+                        }
+                        if (rbNone.isSelected()) {
+                            sum = sum;
+                        }
+                        if (rb5.isSelected()) {
+                            double minus = sum * .05;
+                            sum = sum - minus;
+                        }
+                        if (rb10.isSelected()) {
+                            double minus = sum * .1;
+                            sum = sum - minus;
+                        }
+                        if (rb15.isSelected()) {
+                            double minus = sum * .15;
+                            sum = sum - minus;
+                        }
+                        if (!cPizza.isSelected() && !cTea.isSelected() && !cSundae.isSelected() && !cFries.isSelected() && !cBurger.isSelected() && !cSoftDrinks.isSelected()) {
+                            throw new IllegalSelectorException();
+                        }
+                    } catch (IllegalSelectorException ex) {
+                        JOptionPane.showMessageDialog(null, "No order selected!");
+                        break;
+                    }
+                    JOptionPane.showMessageDialog(null, String.format("%.2f", sum));
+                }while(bool = false);
             }
         });
     }
